a6a879889729d579297935598d26b985f900ef29
Syohei YOSHIDA <shohei.yoshida@dena.com>
2016-09-09 09:54:11 +0900

    Bump up version and update Changes

--- go-add-tags.el
+++ go-add-tags.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-go-add-tags
-;; Version: 0.01
+;; Version: 0.02
 ;; Package-Requires: ((emacs "24") (s "1.11.0") (cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
